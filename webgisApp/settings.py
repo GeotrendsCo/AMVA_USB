b9044c2f48fe892bb912b95b5466f23e9b3ec493
alumno2016 <bateroteso@hotmail.com>
2024-06-27 14:18:30 -0500

    huge maps

--- webgisApp/settings.py
+++ webgisApp/settings.py
@@ -132,3 +132,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Generated huge maps
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
